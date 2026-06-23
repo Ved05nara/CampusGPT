e30f0fd3f6e2c5ee5812b654f8811a5ac8373013
Ved05nara <vedant.narayan@somaiya.edu>
2026-06-23 11:17:43 +0530

    added embeddings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes.upload import router as upload_router
-
+from app.services.pdf_service import extract_text_from_pdf
 app = FastAPI()
 
 app.include_router(upload_router)
--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, UploadFile, File
+from app.services.pdf_service import extract_text_from_pdf
 import os
-
+from app.services.chunk_service import chunk_text
+from app.services.embedding_service import generate_embeddings
 router = APIRouter()
 
 UPLOAD_DIR = "uploads"
@@ -13,7 +15,14 @@ async def upload_pdf(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
 
+    text = extract_text_from_pdf(file_path)
+    chunks = chunk_text(text)
+    embeddings = generate_embeddings(chunks)
     return {
         "message": "File uploaded successfully",
-        "filename": file.filename
+        "filename": file.filename,
+        "characters": len(text),
+        "total_chunks": len(chunks),
+        "first_chunk": chunks[0],
+        "embedding_dimension": len(embeddings[0])
     }
